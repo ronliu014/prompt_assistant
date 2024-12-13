49be9fec439dc753e55ccb366938a9e3dbbdb469
ronliu014 <66141975@qq.com>
2024-12-13 15:29:21 +0800

    add PythonDeveloperAssistant and WebDeveloperAssistant into init file.

--- __init__.py
+++ __init__.py
@@ -2,8 +2,10 @@ from exts import LanguageExpertAssistant
 from exts import ProductDesignerAssistant
 from exts import PyArchitectAssistant
 from exts import PyCoderAssistant
+from exts import PythonDeveloperAssistant
 from exts import SoftwareArchitectAssistant
 from exts import SoftwareEngineerAssistant
+from exts import WebDeveloperAssistant
 from core import PromptAssistant
 from core import PromptTemplateModel
 
@@ -18,8 +20,10 @@ __all__ = [
     "ProductDesignerAssistant",
     "PyArchitectAssistant",
     "PyCoderAssistant",
+    "PythonDeveloperAssistant",
     "SoftwareArchitectAssistant",
     "SoftwareEngineerAssistant",
+    "WebDeveloperAssistant",
     "PromptAssistant",
     "PromptTemplateModel",
     "get_version"
